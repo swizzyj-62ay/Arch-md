e09e945d4a9fc72d79f37d6ee8dbfac2fbc5c7d4
swizzyj-62ay <ihuarulamkenneth@gmail.com>
2025-03-03 23:44:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@ const { version } = require("./package.json")
 require('dotenv').config(); 
 
 //======= Change settings =======\\
-global.owner = process.env.OWNER_NUMBER || "2347036214381"
-global.botname = process.env.BOT_NAME || "αrch md"
+global.owner = process.env.OWNER_NUMBER || "2348093538971"
+global.botname = process.env.BOT_NAME || "swizz"
 global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
 global.prefix = process.env.PREFIX || "."; // your desired prefix symbol only
 global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
